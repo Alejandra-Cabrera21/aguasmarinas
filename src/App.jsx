bffe93753af8d0caa0198ea41f242ab6282a3aaa
Alejandra-Cabrera21 <124795117+Alejandra-Cabrera21@users.noreply.github.com>
2025-04-03 13:59:41 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,18 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Blog />} />
+        <Route path="/" element={<Home />} /> {/* Muestra la página inicial */}
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/tienda" element={<Tienda />} />
         <Route path="/productos/sal-industrial" element={<Producto name="Sal Industrial" />} />
         <Route path="/productos/sal-blanca-yodada" element={<Producto name="Sal Blanca Yodada" />} />
         <Route path="/productos/sal-ganado" element={<Producto name="Sal de Ganado" />} />
-        <Route path="/productos/sal-refinada-b" element={<Producto name="Sal Refinada Tipo B" />} />
-        <Route path="/productos/sal-la-fina" element={<Producto name="Sal Refinada 'La Fina'" />} />
+        <Route path="/productos/sal-refinada-tipo-b" element={<Producto name="Sal Refinada Tipo B" />} />
+        <Route path="/productos/sal-refinada-la-fina" element={<Producto name="Sal Refinada 'La Fina'" />} />
+        <Route path="/productos/sal-yodada-artesana" element={<Producto name="Sal Yodada Artesana" />} />
+
+        {/* Redirección para rutas no encontradas */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
