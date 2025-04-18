b7fb4c55c43cde9bd31f799583ab9bca17377efe
Alejandra-Cabrera21 <124795117+Alejandra-Cabrera21@users.noreply.github.com>
2025-04-17 23:49:57 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // <- esto asegura que los recursos carguen correctamente desde cualquier dominio
   plugins: [react()],
-  base: '/AguasMarinas/', // 👈 nombre exacto del repositorio (sensible a mayúsculas)
-  build: {
-    outDir: 'dist' // 👈 carpeta donde se genera el sitio al hacer build
-  }
 })
